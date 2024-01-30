ffbe3cd6c1b4691f4009d5c82c738d4906dbf7a4
sakhawat-code-info <sakhawat.code.info@gmail.com>
2024-01-30 16:58:08 +0600

    problem solving and questiona added

--- script/loopPractic.js
+++ script/loopPractic.js
@@ -27,10 +27,37 @@ Write a loop 1 to 200. Use break to exit the loop once you find 100.
 Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
 */
 
+let sum = 0, i=1;
+// for (let i = 1; i < 100; i++) {
+//     if(sum >= 100){
+//         break;
+//     }
+//     sum += i;
+//     console.log(sum);
+// }
+
+// while(i < 100){
+//     if (sum > 100) {
+//         break;
+//     }
+//     sum += i;
+//     i++;
+//     console.log(sum);
+// }
+
 
 /* 
 Write a loop that goes from 1 to 100, but stops (using break) when it encounters the first square number (like 4, 9, 16, etc.)
 */
+
+
+
+
+
+
+
+
+
 
 
 /* 
