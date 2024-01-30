20fc0146c3e3e6545bd06c4cd694b2f1d3d87ca5
sakhawat-code-info <sakhawat.code.info@gmail.com>
2024-01-30 16:30:19 +0600

    question added

---
+++ script/loopPractic.js
@@ -0,0 +1,167 @@
+/* 
+
+Write a loop 1 to 200. Use break to exit the loop once you find 100.
+
+*/
+
+
+// for (let i = 1; i <= 200; i++) {
+//     if (i == 100) {
+//         break;
+//     }
+//     console.log(i);
+    
+// }
+
+// let i = 1;
+
+// while (i <= 200) {
+//     if (i == 100) {
+//         break;
+//     }
+//     console.log(i);
+//     i++;
+// }
+
+/* 
+Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
+*/
+
+
+/* 
+Write a loop that goes from 1 to 100, but stops (using break) when it encounters the first square number (like 4, 9, 16, etc.)
+*/
+
+
+/* 
+Write a loop to print even numbers from 1 to 40. Use continue to skip odd numbers.
+*/
+
+/*
+display odd number from 55 to 85 and skip the numbers divisible by 5.
+*/
+
+
+// for 
+/***
+
+"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+
+ */
+/***
+
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+
+ */
+
+
+/***
+
+Subtask-2:
+
+Find all the even numbers from 78 to 98.
+
+ */
+
+/*programming hero*/
+
+
+/***
+
+Subtask-1:
+
+Display sum of all the odd numbers from 91 to 129.
+
+ */
+/***
+
+Subtask-2:
+
+Display sum of all the even numbers from 51 to 85.
+
+ */
+
+/*programming hero*/
+
+/***
+
+Generate a multiplication table for number 9
+
+ */
+
+
+/*programming hero*/
+
+
+/***
+
+Implement a countdown timer that counts down from 81 to 65.
+
+ */
+
+
+/*programming hero*/
+
+/***
+
+"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+
+ */
+
+/*programming hero*/
+/***
+
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+
+ */
+
+
+/***
+
+Subtask-2:
+
+Find all the even numbers from 78 to 98.
+
+ */
+
+/*programming hero*/
+
+
+/***
+
+Subtask-1:
+
+Display sum of all the odd numbers from 81 to 131.
+
+ */
+/***
+
+Subtask-2:
+
+Display sum of all the even numbers from 206 to 311.
+
+ */
+
+/*programming hero*/
+/***
+
+As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
+
+ */
+
+
+/*programming hero*/
+/***
+
+Implement a countdown timer that counts down from 21 to 15.
+
+ */
+
+
+/*programming hero*/
+
+
